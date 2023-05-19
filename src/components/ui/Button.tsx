37b2387c1d6d17977ab19682ed9f9c840d9b969b
root <root@DESKTOP-4GUT5LC.localdomain>
2023-05-19 22:15:21 +0530

    created button variants. created logo

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,42 @@
+import { ButtonHTMLAttributes, FC } from 'react'
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import Loader2 from 'lucide-react'
+
+
+const buttonVariants = cva(
+    'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-baloo font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-700 focus:ring-primary disabled:opacity-50 disabled:pointer-events-none',
+    {
+        variants: {
+            variant: {
+                default: 'bg-secondary text-white hover:bg-pink-700 ',
+                ghost: 'bg-transparent text-slate-900 hover:bg-slate-700 hover:text-slate-400',
+            },
+            size: {
+                default: 'h-10 py-2 px-4',
+                sm: 'h-9 px-2',
+                lg: 'h-11 px-8'
+            }
+        },
+        defaultVariants: {
+            variant: 'default',
+            size: 'default'
+        }
+    },
+)
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+    isLoading?: boolean
+
+}
+
+const Button: FC<ButtonProps> = ({className, children, variant, isLoading, size, ...props}) => {
+  return <button className={cn(buttonVariants({variant, size,className}))} disabled={isLoading}  {...props} >
+    {
+        isLoading ? <Loader2  className="mr-2 h-4 w-4 animate-spin"/> : null
+    }
+    {children}
+  </button>
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+          'baloo' : ['Baloo Bhaijaan 2', 'cursive'],
+          'bungeeHairline' : ['Bungee Hairline', 'cursive'],
+          'bungeeInline' : ['Bungee Inline', 'cursive'],
+          'freckleFace' : ['Freckle Face', 'cursive']     
+      },
+      colors: {
+        'primary': '#00FFFF',
+        'secondary': '#FF00FF',
+        'accent': '#00FF00',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'bgcolor': 'radial-gradient(#6600FF, #9933FF)',
       },
     },
   },
